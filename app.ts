93ac8c287ccb53596af90d5e3bd1f8a8d416e23b
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-07 22:46:21 +0100

    type tuple we can specify type in array

---
+++ app.ts
@@ -0,0 +1,27 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
+    name: 'Maximilain',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+}
+
+// person.role.push('admin')
+// person.role[1] = 10
+// person.role[1, 'Szymon', 'sss']
+
+let favoriteActivity: string[]
+favoriteActivity = ['Sports']
+console.log(person.name)
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
