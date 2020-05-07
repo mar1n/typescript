096b1c9f99e36f09a9ecfa8997e4150b61639eaf
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-08 00:08:37 +0100

    combine types

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-//     name: 'Maximilain',
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// }
-// const Admin = 0
-// const READ_ONLY = 1
-// const AUTHOR = 2
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Maximilain',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin')
-// person.role[1] = 10
-// person.role[1, 'Szymon', 'sss']
-var favoriteActivity;
-favoriteActivity = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    // } else {
+    //     return result.toString()
+    // }
 }
-if (person.role === Role.AUTHOR) {
-    console.log('Admin');
-}
+var combineAges = combine(30, 36, 'as-number');
+console.log(combineAges);
+var combineStringAges = combine('30', '36', 'as-number');
+var combineNames = combine('Max', 'Anna', 'as-text');
+console.log(combineNames);
