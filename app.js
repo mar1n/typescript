fae3d6d71f7be11799bd0b9ab8ef3d4bf1f6e29a
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-08 17:46:08 +0100

    functions types

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //     return +result
-    // } else {
-    //     return result.toString()
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 36, 'as-number');
-console.log(combineAges);
-var combineStringAges = combine('30', '36', 'as-number');
-var combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    return cb(result);
+}
+printResult(add(5, 12));
+var combineValue;
+combineValue = add;
+//combineValue = printResult
+//combineValue = 5
+console.log(combineValue(8, 7));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
